5549239486ae51a5fa9e39414f22009bdb4792f2
gguilt <58526116+gguilt@users.noreply.github.com>
2021-04-11 00:34:17 +0300

    Added argument support for output count

--- probable-octo-broccoli.py
+++ probable-octo-broccoli.py
@@ -1,3 +1,4 @@
+import argparse
 import random
 
 def get_rline(file_name):
@@ -10,9 +11,15 @@ def get_rline(file_name):
     return word.rstrip()
 
 if __name__ == "__main__":
+    # Handle arguments
+    parser = argparse.ArgumentParser("probable-octo-broccoli")
+    parser.add_argument('-c', '--count', help="Count of names to generate. Default is 10.", type=int, default=10)
+    args = parser.parse_args()
+
+    # Do things
     nouns = "nouns.txt"
     adjectives = "adjectives.txt"
 
-    for i in range(10):
+    for i in range(args.count):
         name = get_rline(adjectives) + "-" + get_rline(nouns)
         print(name)
