e392f40e31150bbc9eb7dc601ee7535d18b3d1a7
gguilt <58526116+gguilt@users.noreply.github.com>
2021-04-11 01:01:05 +0300

    Added arguments for nouns and adjectives files

--- probable-octo-broccoli.py
+++ probable-octo-broccoli.py
@@ -10,11 +10,13 @@ if __name__ == "__main__":
     # Handle arguments
     parser = argparse.ArgumentParser("probable-octo-broccoli")
     parser.add_argument('-c', '--count', help="Count of names to generate. Default is 10.", type=int, default=10)
+    parser.add_argument('-fn', '--nouns', help="Nouns file. Default is 'nouns.txt'", type=open, default="nouns.txt")
+    parser.add_argument('-fa', '--adjectives', help="Adjective file. Default is 'adjectives.txt'", type=open, default="adjectives.txt")
     args = parser.parse_args()
 
     # Read files
-    nouns = open("nouns.txt", "r").readlines()
-    adjectives = open("adjectives.txt", "r").readlines()
+    nouns = args.nouns.readlines()
+    adjectives = args.adjectives.readlines()
 
     # Print random lines
     for i in range(args.count):
