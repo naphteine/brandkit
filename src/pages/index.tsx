c2f9f605d33da3be766e7d7d0f1792814abdabac
gguilt <58526116+naphteine@users.noreply.github.com>
2023-03-10 16:34:27 +0300

    Added random font and color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,34 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { SetStateAction, useState } from "react";
+import randomColor from "randomcolor";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
   const [brandName, setBrandName] = useState("Brandkit");
+  const [spacing, setSpacing] = useState(0.1);
+
+  const fonts = [
+    "Gloock",
+    "Outfit",
+    "Archivo",
+    "VT323",
+    "Tilt Neon",
+    "Bebas Neue",
+    "Josefin Sans",
+  ];
 
   const nameChange = (event: { target: { value: SetStateAction<string> } }) => {
     setBrandName(event.target.value);
+  };
+
+  const spacingChange = (event: any) => {
+    setSpacing(event.target.value);
+  };
+
+  const randomFont = () => {
+    return fonts[Math.floor(Math.random() * fonts.length)];
   };
 
   return (
@@ -39,23 +59,128 @@ const Home = () => {
           <br />
 
           <label htmlFor="spacing">Letter spacing</label>
-          <input type="range" id="spacing" name="spacing" />
+          <input
+            type="range"
+            id="spacing"
+            name="spacing"
+            min={-20}
+            max={20}
+            value={spacing}
+            onChange={spacingChange}
+          />
         </section>
 
         <h2>Results</h2>
         <section className={styles.resultbox} title="results">
-          <article className={styles.gloock}>{brandName}</article>
-          <article className={styles.outfit}>{brandName}</article>
-          <article className={styles.archivo}>{brandName}</article>
-          <article className={styles.vt323}>{brandName}</article>
-          <article className={styles.gloock}>{brandName}</article>
-          <article className={styles.outfit}>{brandName}</article>
-          <article className={styles.archivo}>{brandName}</article>
-          <article className={styles.vt323}>{brandName}</article>
-          <article className={styles.gloock}>{brandName}</article>
-          <article className={styles.outfit}>{brandName}</article>
-          <article className={styles.archivo}>{brandName}</article>
-          <article className={styles.vt323}>{brandName}</article>
+          {}
+          <article
+            style={{
+              backgroundColor: randomColor(),
+              letterSpacing: spacing + "px",
+              fontFamily: randomFont(),
+            }}
+          >
+            {brandName}
+          </article>
+          <article
+            style={{
+              backgroundColor: randomColor(),
+              letterSpacing: spacing + "px",
+              fontFamily: randomFont(),
+            }}
+          >
+            {brandName}
+          </article>
+          <article
+            style={{
+              backgroundColor: randomColor(),
+              letterSpacing: spacing + "px",
+              fontFamily: randomFont(),
+            }}
+          >
+            {brandName}
+          </article>
+          <article
+            style={{
+              backgroundColor: randomColor(),
+              letterSpacing: spacing + "px",
+              fontFamily: randomFont(),
+            }}
+          >
+            {brandName}
+          </article>
+          <article
+            style={{
+              backgroundColor: randomColor(),
+              letterSpacing: spacing + "px",
+              fontFamily: randomFont(),
+            }}
+          >
+            {brandName}
+          </article>
+          <article
+            style={{
+              backgroundColor: randomColor(),
+              letterSpacing: spacing + "px",
+              fontFamily: randomFont(),
+            }}
+          >
+            {brandName}
+          </article>
+          <article
+            style={{
+              backgroundColor: randomColor(),
+              letterSpacing: spacing + "px",
+              fontFamily: randomFont(),
+            }}
+          >
+            {brandName}
+          </article>
+          <article
+            style={{
+              backgroundColor: randomColor(),
+              letterSpacing: spacing + "px",
+              fontFamily: randomFont(),
+            }}
+          >
+            {brandName}
+          </article>
+          <article
+            style={{
+              backgroundColor: randomColor(),
+              letterSpacing: spacing + "px",
+              fontFamily: randomFont(),
+            }}
+          >
+            {brandName}
+          </article>
+          <article
+            style={{
+              backgroundColor: randomColor(),
+              letterSpacing: spacing + "px",
+              fontFamily: randomFont(),
+            }}
+          >
+            {brandName}
+          </article>
+          <article
+            style={{
+              backgroundColor: randomColor(),
+              letterSpacing: spacing + "px",
+              fontFamily: randomFont(),
+            }}
+          >
+            {brandName}
+          </article>
+          <article
+            style={{
+              backgroundColor: randomColor(),
+              letterSpacing: spacing + "px",
+              fontFamily: randomFont(),
+            }}
+          >
+            {brandName}
+          </article>
         </section>
       </main>
     </>
